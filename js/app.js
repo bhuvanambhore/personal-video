819b8405840b8788875d963f66f199110381a0cb
bhuvanambhore <bhuvanmambhore@gmail.com>
2026-07-31 21:48:54 +0530

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,42 @@
+const videos = [
+
+{
+title:"Vacation",
+url:"#"
+},
+
+{
+title:"Family",
+url:"#"
+},
+
+{
+title:"Tutorial",
+url:"#"
+}
+
+];
+
+const list=document.getElementById("videoList");
+
+videos.forEach(video=>{
+
+const card=document.createElement("div");
+
+card.className="card";
+
+card.innerHTML=`
+
+<h3>${video.title}</h3>
+
+<button onclick="alert('Video player coming in Step 4')">
+
+Play Video
+
+</button>
+
+`;
+
+list.appendChild(card);
+
+});
